a128eaa93bad45056a15ead80545af3e093ce07c
carlos <carloshvronchi@gmail.com>
2023-01-20 17:37:41 +0100

    add slides with a different header

--- quarto_render.sh
+++ quarto_render.sh
@@ -2,4 +2,10 @@ R -e "quarto::quarto_render(output_format = 'all')"
 
 # insert assets into the website as well
 folder_name=20230126_workshop
+rm -r docs/$folder_name
 cp -r assets/$folder_name/results docs/$folder_name
+# add <a href="./"></a> to the top of the file
+awk 'NR==2 {print "<a href=\"./\"></a>"} 1' docs/$folder_name/slides.html > \
+    docs/$folder_name/index.html
+# remove slides.html
+rm docs/$folder_name/slides.html
